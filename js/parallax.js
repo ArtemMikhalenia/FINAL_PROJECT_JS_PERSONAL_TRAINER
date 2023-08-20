4176bee037f1b02fb9de05f5454f475ab5f50a58
Artem <goldtree3000@gmail.com>
2023-08-20 14:55:21 +0300

    adaptive main page

--- js/parallax.js
+++ js/parallax.js
@@ -1,18 +1,24 @@
 class Parallax {
-   //parallax
-   constructor(container) {
-      this.container = container;
+   constructor() {
+      this.myContainer = null;
+      this.woman = null;
+      this.man = null;
+      this.fog = null;
    }
 
-   // woman: document.querySelector('.woman-image'),
-   // man: document.querySelector('.man-image'),
-   // fog: document.querySelector('.fog-image'),
+
+   init(container, img1, img2, img3) {
+      this.myContainer = container;
+      this.woman = img1;
+      this.man = img2;
+      this.fog = img3;
+      this.parallax();
+   }
 
    parallax() {
-      if (this.container) {
-         const woman = document.querySelector('.woman-image');
-         const man = document.querySelector('.man-image');
-         const fog = document.querySelector('.fog-image');
+      if (this.myContainer) {
+         const containerWidth = this.myContainer.offsetWidth;
+         const containerHeight = this.myContainer.offsetHeight;
 
          //коэффициенты
          const forMan = 30;
@@ -28,39 +34,33 @@ class Parallax {
          let coordXPercent = 0;
          let coordYPercent = 0;
 
-         function parallaxFunction() {
+         const parallaxFunction = () => {
             const distX = coordXPercent - posX;
             const distY = coordYPercent - posY;
 
             posX = posX + (distX * speed);
             posY = posY + (distY * speed);
 
-            man.style.cssText = `transform: translate(${posX / forMan}%,${posY / forMan}%);`;
-            woman.style.cssText = `transform: translate(${posX / forWoman}%,${posY / forWoman}%);`;
-            fog.style.cssText = `transform: translate(${posX / forFog}%,${posY / forFog}%);`;
+            this.man.style.cssText = `transform: translate(${posX / forMan}%,${posY / forMan}%);`;
+            this.woman.style.cssText = `transform: translate(${posX / forWoman}%,${posY / forWoman}%);`;
+            this.fog.style.cssText = `transform: translate(${posX / forFog}%,${posY / forFog}%);`;
 
             requestAnimationFrame(parallaxFunction);
          }
 
-         parallaxFunction();
-
-         this.container.addEventListener("mousemove", function (e) {
-
-            //Получение ширины и высоты блока
-            const parallaxBlockWidth = this.container.offsetWidth;
-            const parallaxBlockHeigth = this.container.offsetHeight;
-            //Ноль на середине
-            const coordX = e.pageX - parallaxBlockWidth / 2;
-            const coordY = e.pageY - parallaxBlockHeigth / 2;
+         this.myContainer.addEventListener("mousemove", function (el) {
+            // Ноль на середине
+            const coordX = el.pageX - containerWidth / 2;
+            const coordY = el.pageY - containerHeight / 2;
 
             //Получаем проценты
-            coordXPercent = coordX / parallaxBlockWidth * 100;
-            coordYPercent = coordY / parallaxBlockHeigth * 100;
+            coordXPercent = coordX / containerWidth * 100;
+            coordYPercent = coordY / containerHeight * 100;
          });
+
+         parallaxFunction();
       }
    }
 }
 
-let parallax = new Parallax(document.querySelector('.wrapper'));
-
-export default parallax;
+export default Parallax;
--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+//import
 import Parallax from './parallax.js';
 
-Parallax.parallax();
+const container = document.querySelector('.wrapper');
+const woman = document.querySelector('.woman-image');
+const man = document.querySelector('.man-image');
+const fog = document.querySelector('.fog-image');
+
+const parallax = new Parallax();
+parallax.init(container, woman, man, fog);
+
+// parallax.parallax();
